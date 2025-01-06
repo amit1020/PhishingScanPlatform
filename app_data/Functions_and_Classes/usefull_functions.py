7cf3289b24242049f69f3084b43bfaa74af59e1c
Amit1020 <77537926+amit1020@users.noreply.github.com>
2025-01-06 14:15:40 +0200

    Finish Login page with authentication page

--- app_data/Functions_and_Classes/usefull_functions.py
+++ app_data/Functions_and_Classes/usefull_functions.py
@@ -54,3 +54,33 @@ def Enter_cvsv_to_file(file_path, data):
             
             
             
+            
+            
+"""  2FA example code 
+    from flask import Flask, request, session
+    import pyotp
+
+    app = Flask(__name__)
+    app.secret_key = 'your_secret_key_here'
+
+    # יצירת מפתח סודי (במקרה האמיתי יש לאחסן בבסיס נתונים)
+    secret = pyotp.random_base32()
+
+    @app.route('/')
+    def index():
+        if 'otp' in session:
+            return 'Logged in using 2FA!'
+        return 'You are not logged in'
+
+    @app.route('/login', methods=['POST'])
+    def login():
+        user_otp = request.form['otp']
+        if pyotp.TOTP(secret).verify(user_otp):
+            session['otp'] = True
+            return 'You are logged in'
+        return 'Login failed'
+
+    if __name__ == '__main__':
+        app.run(debug=True)
+        
+"""
