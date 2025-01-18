cd2c5647b328c520e97ead44416557b16064ec58
Amit1020 <77537926+amit1020@users.noreply.github.com>
2025-01-18 16:38:04 +0200

    Updated SQL code at Useers_DB.sql

--- app_data/Functions_and_Classes/DB_codes/Users_DB.sql
+++ app_data/Functions_and_Classes/DB_codes/Users_DB.sql
@@ -15,3 +15,42 @@ CREATE TABLE IF NOT EXISTS `Links_Table` (
   `headers` varchar(200) NOT NULL,
   PRIMARY KEY (`Serial_Number`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
+-- Phishing_Database.Users_Technical_Data_Table definition
+
+CREATE TABLE IF NOT EXISTS `Users_Technical_Data_Table` (
+  `userID` varchar(18) DEFAULT NULL,
+  `2FA_key` varchar(32) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `2FA_enabled` tinyint(1) NOT NULL,
+  `last_login` timestamp NULL DEFAULT NULL,
+  `status` enum('active','inactive','banned') NOT NULL DEFAULT 'active',
+  UNIQUE KEY `Users_Technical_Data_Table_UNIQUE` (`2FA_key`),
+  KEY `Users_Technical_Data_Table_Users_FK` (`userID`),
+  CONSTRAINT `Users_Technical_Data_Table_Users_FK` FOREIGN KEY (`userID`) REFERENCES `Users_Table` (`userID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
+
+-- Phishing_Database.Users_Table definition
+CREATE TABLE IF NOT EXISTS `Users_Table` (
+  `userID` varchar(18) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `organization` varchar(100) DEFAULT NULL,
+  `country` varchar(39) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `phone_number` varchar(18) NOT NULL,
+  `street_address` varchar(255) DEFAULT NULL,
+  `city` varchar(100) DEFAULT NULL,
+  `postal_code` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`userID`),
+  UNIQUE KEY `Users_UNIQUE` (`email`),
+  UNIQUE KEY `Users_UNIQUE_1` (`phone_number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
